697b04c7fa9e15aaa25d84a15f932ffce6d0bfa1
Haley Ward <haley.ward@lifeomic.com>
2021-03-08 09:55:29 -0500

    Create unstyed button

---
+++ src/components/ButtonUnstyled/ButtonUnstyled.test.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import { ButtonUnstyled, ButtonUnstyledProps } from './index';
+import { renderWithTheme } from '../../testUtils/renderWithTheme';
+
+const testId = 'ButtonUnstyled';
+
+const getBaseProps = (): ButtonUnstyledProps => ({});
+
+test('it renders an ButtonUnstyled', async () => {
+  const props = getBaseProps();
+  const { findByTestId } = renderWithTheme(
+    <ButtonUnstyled {...props} data-testid={testId}>
+      Button
+    </ButtonUnstyled>
+  );
+  const button = await findByTestId(testId);
+  expect(button).toBeInTheDocument();
+  expect(button.getAttribute('type')).toEqual('button');
+  expect(button.getAttribute('tabIndex')).toEqual('0');
+});
+
+test('it applies the provided className', async () => {
+  const props = getBaseProps();
+  const { findByTestId } = renderWithTheme(
+    <ButtonUnstyled
+      {...props}
+      data-testid={testId}
+      className="custom-class-name"
+    >
+      Button
+    </ButtonUnstyled>
+  );
+  const button = await findByTestId(testId);
+  expect(button).toHaveClass('custom-class-name');
+});
+
+test('it renders with the "fullWidth" prop', async () => {
+  const props = getBaseProps();
+  const { findByTestId } = renderWithTheme(
+    <ButtonUnstyled {...props} data-testid={testId} fullWidth>
+      Button
+    </ButtonUnstyled>
+  );
+  const button = await findByTestId(testId);
+  expect(button).toHaveClass('ChromaButtonUnstyled-fullWidth');
+});
+
+test('it renders a disabled button', async () => {
+  const props = getBaseProps();
+  const { findByTestId } = renderWithTheme(
+    <ButtonUnstyled {...props} data-testid={testId} disabled>
+      Button
+    </ButtonUnstyled>
+  );
+  const button = await findByTestId(testId);
+  expect(button).toBeDisabled();
+  expect(button.getAttribute('tabIndex')).toEqual('-1');
+});
