fb2e9cada5a8a90c467df6af9f2953e37bb9ddc6
Casey Dexter <casey.dexter@lifeomic.com>
2022-06-10 14:47:57 -0400

    feat: Add negative and positive pill to story

--- stories/components/Pill/Pill.stories.tsx
+++ stories/components/Pill/Pill.stories.tsx
@@ -12,6 +12,8 @@ const AllPillsStory: React.FC = () => (
       <Pill label="123 Results" />
       <Pill label="1 of 20" />
       <Pill label="3" color="primary" />
+      <Pill label="4" color="negative" />
+      <Pill label="5" color="positive" />
       <Pill label="Beta" variant="highlight" />
     </Container>
   </>
@@ -51,10 +53,40 @@ const HighlightPillStory: React.FC = () => (
   </>
 );
 
+const NegativePillStory: React.FC = () => (
+  <>
+    <Container
+      containerStyles={{ display: 'flex', justifyContent: 'space-around' }}
+    >
+      <Pill label="123 Results" color="negative" />
+      <Pill label="1 of 20" color="negative" />
+      <Pill label="3" color="negative" />
+    </Container>
+  </>
+);
+
+const PositivePillStory: React.FC = () => (
+  <>
+    <Container
+      containerStyles={{ display: 'flex', justifyContent: 'space-around' }}
+    >
+      <Pill label="123 Results" color="positive" />
+      <Pill label="1 of 20" color="positive" />
+      <Pill label="3" color="positive" />
+    </Container>
+  </>
+);
+
 storiesOf('Components/Pill', module)
   .add('Default', () => <AllPillsStory />, {
     readme: { content: md },
   })
   .add('Highlight', () => <HighlightPillStory />, {
+    readme: { content: md },
+  })
+  .add('Negative', () => <NegativePillStory />, {
+    readme: { content: md },
+  })
+  .add('Positive', () => <PositivePillStory />, {
     readme: { content: md },
   });
