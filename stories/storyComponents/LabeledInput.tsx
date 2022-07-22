925b5e7ba69e94c2db61c51be522db0b31e07457
Sam Yeager <samantha.yeager@lifeomic.com>
2022-07-22 11:12:59 -0700

    chore: replace rgba in one more file

--- stories/storyComponents/LabeledInput.tsx
+++ stories/storyComponents/LabeledInput.tsx
@@ -1,6 +1,30 @@
 import * as React from 'react';
-import black from '../../src/colors/black';
-import { spacing } from '../storyStyles';
+import { makeStyles } from '../../src/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginBottom: theme.spacing(4),
+  },
+  input: {
+    appearance: 'none',
+    borderRadius: '0.25rem',
+    border: `1px solid ${theme.palette.black[100]}`,
+    paddingTop: '0.5rem',
+    paddingBottom: '0.5rem',
+    paddingLeft: '0.75rem',
+    paddingRight: '0.75rem',
+    boxShadow: `
+      0 1px 3px 0 ${theme.hexToRgba(theme.palette.common.black, 0.1)}, 
+      0 1px 2px 0 ${theme.hexToRgba(theme.palette.common.black, 0.06)}
+    `,
+  },
+  label: {
+    display: 'block',
+    marginBottom: theme.spacing(2),
+    fontWeight: 'bold',
+    color: theme.palette.black[700],
+  },
+}));
 
 interface LabeledInputProps {
   label?: string;
@@ -15,17 +39,10 @@ export const LabeledInput: React.FC<LabeledInputProps> = ({
   type = 'number',
   onChange,
 }) => {
+  const classes = useStyles({});
   return (
-    <div style={{ marginBottom: spacing[4] }}>
-      <label
-        htmlFor={label}
-        style={{
-          display: 'block',
-          marginBottom: spacing[2],
-          fontWeight: 'bold',
-          color: black[700],
-        }}
-      >
+    <div className={classes.root}>
+      <label htmlFor={label} className={classes.label}>
         {label}
       </label>
       <input
@@ -34,16 +51,7 @@ export const LabeledInput: React.FC<LabeledInputProps> = ({
         id={label}
         value={value}
         onInput={onChange}
-        style={{
-          appearance: 'none',
-          borderRadius: '0.25rem',
-          border: `1px solid ${black[100]}`,
-          paddingTop: '0.5rem',
-          paddingBottom: '0.5rem',
-          paddingLeft: '0.75rem',
-          paddingRight: '0.75rem',
-          boxShadow: '0 1px 3px 0 rgba(0,0,0,.1), 0 1px 2px 0 rgba(0,0,0,.06)',
-        }}
+        className={classes.input}
       />
     </div>
   );
