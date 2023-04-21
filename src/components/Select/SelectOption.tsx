b4a2297a3bfca6576c82a13d10b88de2de2a88ba
Sam <samantha.yeager@lifeomic.com>
2023-04-21 15:15:57 -0700

    add secondary action to SelectOption

--- src/components/Select/SelectOption.tsx
+++ src/components/Select/SelectOption.tsx
@@ -5,6 +5,7 @@ import { makeStyles } from '../../styles';
 import { GetClasses } from '../../typeUtils';
 import { Box } from '../Box';
 import { Text } from '../Text';
+import { IconButton } from '../IconButton';
 
 export const SelectOptionStylesKey = 'ChromaSelectOption';
 
@@ -48,15 +49,28 @@ export const useStyles = makeStyles(
       cursor: 'not-allowed',
       opacity: 0.625,
     },
+    obviousHover: {
+      '&:hover': {
+        backgroundColor: theme.palette.primary[100],
+      },
+    },
   }),
   { name: SelectOptionStylesKey }
 );
 
 export type SelectOptionClasses = GetClasses<typeof useStyles>;
 
+export type SelectSecondaryAction = {
+  action: (args: any | undefined) => void;
+  args: any | undefined;
+  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  label: string;
+};
+
 export interface SelectOptionProps {
   className?: string;
   children?: React.ReactNode;
+  secondaryAction?: SelectSecondaryAction;
   disabled?: boolean;
   isChecked?: boolean;
   meta?: any;
@@ -68,6 +82,7 @@ export interface SelectOptionProps {
 export const SelectOption: React.FC<SelectOptionProps> = ({
   className,
   children,
+  secondaryAction,
   isChecked,
   title,
   subtitle,
@@ -76,33 +91,46 @@ export const SelectOption: React.FC<SelectOptionProps> = ({
 }) => {
   const classes = useStyles({});
   return (
-    <Box className={clsx(classes.root, className)} fullWidth {...rootProps}>
-      {isChecked && (
-        // This is required for a11y: we need to have some indicator for our screenreader friends
-        <VisuallyHidden>&#x2713;</VisuallyHidden>
+    <Box fullWidth justify="space-between">
+      <Box className={clsx(classes.root, className)} fullWidth {...rootProps}>
+        {isChecked && (
+          // This is required for a11y: we need to have some indicator for our screenreader friends
+          <VisuallyHidden>&#x2713;</VisuallyHidden>
+        )}
+        {title && (
+          <Box
+            direction="column"
+            justify="flex-start"
+            className={clsx(
+              isChecked && classes.checked,
+              disabled && classes.disabled
+            )}
+          >
+            {title && (
+              <Text className={classes.title} size="subbody">
+                {title}
+              </Text>
+            )}
+            {subtitle && (
+              <Text className={classes.subtitle} size="caption">
+                {subtitle}
+              </Text>
+            )}
+          </Box>
+        )}
+        {children}
+      </Box>
+      {secondaryAction && (
+        <IconButton
+          className={classes.obviousHover}
+          aria-label={secondaryAction?.label}
+          icon={secondaryAction?.icon}
+          onClick={(e) => {
+            e.stopPropagation();
+            secondaryAction.action(secondaryAction.args);
+          }}
+        />
       )}
-      {title && (
-        <Box
-          direction="column"
-          justify="flex-start"
-          className={clsx(
-            isChecked && classes.checked,
-            disabled && classes.disabled
-          )}
-        >
-          {title && (
-            <Text className={classes.title} size="subbody">
-              {title}
-            </Text>
-          )}
-          {subtitle && (
-            <Text className={classes.subtitle} size="caption">
-              {subtitle}
-            </Text>
-          )}
-        </Box>
-      )}
-      {children}
     </Box>
   );
 };
