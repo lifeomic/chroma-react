5f03b13bc5108fd514e4a29c5fb380f73de5196f
Sam <samantha.aran@lifeomic.com>
2023-08-22 17:23:54 -0700

    fix: Update C- Component to Storybook CSF3

--- src/components/ColorPicker/ColorPicker.stories.tsx
+++ src/components/ColorPicker/ColorPicker.stories.tsx
@@ -1,181 +1,181 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { StoryObj, Meta } from '@storybook/react';
 
 import { ColorPicker } from './ColorPicker';
 import { HelpCircle } from '@lifeomic/chromicons';
 
-export default {
+const meta: Meta<typeof ColorPicker> = {
   title: 'Form Components/ColorPicker',
   component: ColorPicker,
-  argTypes: {},
-} as ComponentMeta<typeof ColorPicker>;
-
-const Template: ComponentStory<typeof ColorPicker> = (args) => (
-  <ColorPicker {...args} />
-);
-
-export const Default = Template.bind({});
-Default.args = {
-  label: 'Color Picker',
+  args: {
+    label: 'Color Picker',
+  },
+  decorators: [
+    (story) => <div style={{ width: '100%', height: '250px' }}>{story()}</div>,
+  ],
 };
+export default meta;
+type Story = StoryObj<typeof ColorPicker>;
 
-export const InvalidColorText = Template.bind({});
-InvalidColorText.parameters = {
-  docs: {
-    description: {
-      story: 'Displays text in Popover header when color is invalid.',
+export const Default: Story = {};
+
+export const InvalidColorText: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'Displays text in Popover header when color is invalid.',
+      },
     },
   },
-};
-InvalidColorText.args = {
-  label: 'Color Picker',
-  invalidColorText: 'Invalid Color Text',
-};
-
-export const SecondaryLabel = Template.bind({});
-SecondaryLabel.parameters = {
-  docs: {
-    description: {
-      story:
-        'A secondary, supplemental label to display for the ColorPicker element.',
-    },
+  args: {
+    invalidColorText: 'Invalid Color Text',
   },
 };
-SecondaryLabel.args = {
-  label: 'Color Picker',
-  secondaryLabel: 'Secondary Label',
-};
 
-export const Placeholder = Template.bind({});
-Placeholder.parameters = {
-  docs: {
-    description: {
-      story: 'The placeholder text to display.',
+export const SecondaryLabel: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'A secondary, supplemental label to display for the ColorPicker element.',
+      },
     },
   },
-};
-Placeholder.args = {
-  label: 'Color Picker',
-  placeholder: 'Placeholder',
-};
-
-export const IconAndTooltipMessage = Template.bind({});
-IconAndTooltipMessage.parameters = {
-  docs: {
-    description: {
-      story:
-        'An additional icon and tooltip element can be used to provide additional context ' +
-        'for the user on how the text entry will be used.',
-    },
+  args: {
+    secondaryLabel: 'Secondary Label',
   },
 };
-IconAndTooltipMessage.args = {
-  label: 'Color Picker',
-  icon: HelpCircle,
-  tooltipMessage: "If you provide a color, you'll get free tacos!",
-};
 
-export const HelpMessage = Template.bind({});
-HelpMessage.parameters = {
-  docs: {
-    description: {
-      story:
-        'Caption, help text to display underneath the element. This should be ' +
-        'supplemental text to the label, typically an expanded description of the option.',
+export const Placeholder: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'The placeholder text to display.',
+      },
     },
   },
-};
-HelpMessage.args = {
-  label: 'Color Picker',
-  helpMessage: 'Some helper text!',
-};
-
-export const ShowRequiredLabel = Template.bind({});
-ShowRequiredLabel.parameters = {
-  docs: {
-    description: {
-      story:
-        'Shows an `*` next to the label; required validation must be handled separately.',
-    },
+  args: {
+    placeholder: 'Placeholder',
   },
 };
-ShowRequiredLabel.args = {
-  label: 'Color Picker',
-  showRequiredLabel: true,
-};
 
-export const HasError = Template.bind({});
-HasError.parameters = {
-  docs: {
-    description: {
-      story: 'Sets an error style on the element.',
+export const IconAndTooltipMessage: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'An additional icon and tooltip element can be used to provide additional context ' +
+          'for the user on how the text entry will be used.',
+      },
     },
   },
-};
-HasError.args = {
-  label: 'Color Picker',
-  hasError: true,
-};
-
-export const ErrorMessage = Template.bind({});
-ErrorMessage.parameters = {
-  docs: {
-    description: {
-      story:
-        'Caption, error text to display underneath the element when an error occurs. For ' +
-        'the message to be displayed `hasError` must be set as well.',
-    },
+  args: {
+    icon: HelpCircle,
+    tooltipMessage: "If you provide a color, you'll get free tacos!",
   },
 };
-ErrorMessage.args = {
-  label: 'Color Picker',
-  hasError: true,
-  errorMessage: 'Error Message',
-};
 
-export const Disabled = Template.bind({});
-Disabled.parameters = {
-  docs: {
-    description: {
-      story:
-        'Applies the disabled state to the element.\n\n' +
-        '**REMINDER:** If you use `disabled`, screenreaders will not announce the text ' +
-        'inside of the ColorPicker to the user, and will completely skip over this element.',
+export const HelpMessage: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Caption, help text to display underneath the element. This should be ' +
+          'supplemental text to the label, typically an expanded description of the option.',
+      },
     },
   },
-};
-Disabled.args = {
-  label: 'Color Picker',
-  disabled: true,
-};
-
-export const ReadOnly = Template.bind({});
-ReadOnly.parameters = {
-  docs: {
-    description: {
-      story: 'Applies the read only state to the element.',
-    },
+  args: {
+    helpMessage: 'Some helper text!',
   },
 };
-ReadOnly.args = {
-  label: 'Color Picker',
-  readOnly: true,
+
+export const ShowRequiredLabel: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Shows an `*` next to the label; required validation must be handled separately.',
+      },
+    },
+  },
+  args: {
+    showRequiredLabel: true,
+  },
 };
 
-export const InverseDark = Template.bind({});
-InverseDark.parameters = {
-  backgrounds: { default: 'dark' },
-};
-InverseDark.args = {
-  label: 'Color Picker',
-  color: 'inverse',
+export const HasError: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'Sets an error style on the element.',
+      },
+    },
+  },
+  args: {
+    hasError: true,
+  },
 };
 
-export const InverseBlue = Template.bind({});
-InverseBlue.parameters = {
-  backgrounds: { default: 'blue' },
+export const ErrorMessage: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Caption, error text to display underneath the element when an error occurs. For ' +
+          'the message to be displayed `hasError` must be set as well.',
+      },
+    },
+  },
+  args: {
+    hasError: true,
+    errorMessage: 'Error Message',
+  },
 };
-InverseBlue.args = {
-  label: 'Color Picker',
-  color: 'inverse',
+
+export const Disabled: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Applies the disabled state to the element.\n\n' +
+          '**REMINDER:** If you use `disabled`, screenreaders will not announce the text ' +
+          'inside of the ColorPicker to the user, and will completely skip over this element.',
+      },
+    },
+  },
+  args: {
+    disabled: true,
+  },
+};
+
+export const ReadOnly: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: 'Applies the read only state to the element.',
+      },
+    },
+  },
+  args: {
+    readOnly: true,
+  },
+};
+
+export const InverseDark: Story = {
+  parameters: {
+    backgrounds: { default: 'dark' },
+  },
+  args: {
+    color: 'inverse',
+  },
+};
+
+export const InverseBlue: Story = {
+  parameters: {
+    backgrounds: { default: 'blue' },
+  },
+  args: {
+    color: 'inverse',
+  },
 };
