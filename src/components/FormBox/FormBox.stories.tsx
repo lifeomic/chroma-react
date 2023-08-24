5fadb8974b3e947ce7c20dd85f55058c47202f27
Sam <samantha.aran@lifeomic.com>
2023-08-23 17:15:19 -0700

    fix: Update E- to H- Components to Storybook CSF3

--- src/components/FormBox/FormBox.stories.tsx
+++ src/components/FormBox/FormBox.stories.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { StoryObj, StoryFn, Meta } from '@storybook/react';
 
 import { FormBox } from './FormBox';
 import { TextField } from '../TextField';
 import { TextArea } from '../TextArea';
 
-export default {
+const meta: Meta<typeof FormBox> = {
   title: 'Form Components/FormBox',
   component: FormBox,
   argTypes: {},
-} as ComponentMeta<typeof FormBox>;
+};
+export default meta;
+type Story = StoryObj<typeof FormBox>;
 
-const Template: ComponentStory<typeof FormBox> = (args) => (
+const Template: StoryFn<typeof FormBox> = (args) => (
   <FormBox {...args}>
     <TextField label="Text Field" />
     <TextArea label="Text Area" />
   </FormBox>
 );
 
-export const Default = Template.bind({});
-Default.args = {};
+export const Default: Story = {
+  render: Template,
+};
