be3036403d776cc4bd20a54b1efdb2bf96a359b9
Sam <samantha.aran@lifeomic.com>
2023-08-24 12:46:01 -0700

    fix height and add stories for IBFloat

--- src/components/IconButtonFloat/IconButtonFloat.stories.tsx
+++ src/components/IconButtonFloat/IconButtonFloat.stories.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StoryObj, Meta } from '@storybook/react';
 
 import { IconButtonFloat } from './IconButtonFloat';
-import { Edit } from '@lifeomic/chromicons';
+import { Edit, Settings } from '@lifeomic/chromicons';
 
 const meta: Meta<typeof IconButtonFloat> = {
   component: IconButtonFloat,
@@ -22,19 +22,21 @@ const meta: Meta<typeof IconButtonFloat> = {
   },
   decorators: [
     (story) => (
-      <div
-        style={{
-          position: 'fixed',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-        }}
-      >
-        {story()}
+      <div style={{ height: '50px' }}>
+        <div
+          style={{
+            position: 'fixed',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+          }}
+        >
+          {story()}
+        </div>
       </div>
     ),
   ],
@@ -43,6 +45,32 @@ export default meta;
 type Story = StoryObj<typeof IconButtonFloat>;
 
 export const Default: Story = {};
+
+export const Icon: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'For a list of available icons, see our <a href="https://lifeomic.github.io/chromicons.com/">Chrōmicons catalog</a>.',
+      },
+    },
+  },
+  args: {
+    icon: Settings,
+  },
+};
+
+export const Size: Story = {
+  args: {
+    size: 0,
+  },
+};
+
+export const Justify: Story = {
+  args: {
+    justify: 'left',
+  },
+};
 
 export const Disabled: Story = {
   args: {
