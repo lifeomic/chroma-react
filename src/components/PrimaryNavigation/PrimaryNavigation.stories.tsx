f1a193187f322590f15ff6e0cc12961735b4d88a
Sam <samantha.aran@lifeomic.com>
2023-08-29 16:20:37 -0700

    fix: Update first half of P- Components to Storybook CSF3

--- src/components/PrimaryNavigation/PrimaryNavigation.stories.tsx
+++ src/components/PrimaryNavigation/PrimaryNavigation.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { StoryObj, Meta } from '@storybook/react';
 
 import { PrimaryNavigation } from './PrimaryNavigation';
 import { PrimaryNavigationItem } from './PrimaryNavigationItem';
@@ -14,10 +14,8 @@ import { PrimaryNavigationSubItem } from './PrimaryNavigationSubItem';
 (LayoutManagerContext.Provider as any).displayName =
   'LayoutManagerContext.Provider';
 
-export default {
-  title: 'Components/PrimaryNavigation',
+const meta: Meta<typeof PrimaryNavigation> = {
   component: PrimaryNavigation,
-  argTypes: {},
   decorators: [
     (story) => {
       const [isSidebarCollapsed, setIsSidebarCollapsed] = React.useState(false);
@@ -37,113 +35,117 @@ export default {
     },
     (story) => <MemoryRouter>{story()}</MemoryRouter>,
   ],
-} as ComponentMeta<typeof PrimaryNavigation>;
-
-export const Default: ComponentStory<typeof PrimaryNavigation> = (args) => {
-  return (
-    <PrimaryNavigation {...args}>
-      <PrimaryNavigationItem to="/edit" icon={<Edit />} label="Nav Item" />
-      <PrimaryNavigationItem
-        to="/right"
-        icon={<ArrowRight />}
-        label="Nav Item 2"
-        betaLabelText="Beta"
-      />
-    </PrimaryNavigation>
-  );
 };
-Default.args = {
-  header: (
-    <div
-      style={{
-        color: 'white',
-        display: 'flex',
-        justifyContent: 'center',
-        width: '100%',
-        border: '1px dashed white',
-      }}
-    >
-      <Text color="inverse">Optional Header Section</Text>
-    </div>
-  ),
-  footer: (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        color: 'white',
-        width: '100%',
-        border: '1px dashed white',
-      }}
-    >
-      <Text color="inverse">Optional Footer Section</Text>
-    </div>
-  ),
-  headerMin: <b>MIN</b>,
-};
+export default meta;
+type Story = StoryObj<typeof PrimaryNavigation>;
 
-export const PrimaryNavigationSubrouting: ComponentStory<typeof PrimaryNavigation> = (
-  args
-) => {
-  return (
-    <PrimaryNavigation {...args}>
-      <PrimaryNavigationItem to="/edit" icon={<Edit />} label="Nav Item" />
-      <PrimaryNavigationItem
-        to="/left"
-        icon={<ArrowLeft />}
-        label="Nav Item 2"
-        betaLabelText="Beta"
-      />
-      <PrimaryNavigationExpansionItem
-        icon={<Subjects />}
-        label="Base Route"
-        to="/base"
-        rootParentPath="/base"
+export const Default: Story = {
+  render: (args) => {
+    return (
+      <PrimaryNavigation {...args}>
+        <PrimaryNavigationItem to="/edit" icon={<Edit />} label="Nav Item" />
+        <PrimaryNavigationItem
+          to="/right"
+          icon={<ArrowRight />}
+          label="Nav Item 2"
+          betaLabelText="Beta"
+        />
+      </PrimaryNavigation>
+    );
+  },
+  args: {
+    header: (
+      <div
+        style={{
+          color: 'white',
+          display: 'flex',
+          justifyContent: 'center',
+          width: '100%',
+          border: '1px dashed white',
+        }}
       >
-        <PrimaryNavigationSubItem label="Child Route1" to="/base/c1" />
-        <PrimaryNavigationSubItem label="Child Route2" to="/base/c2" />
-      </PrimaryNavigationExpansionItem>
-    </PrimaryNavigation>
-  );
-};
-PrimaryNavigationSubrouting.parameters = {
-  docs: {
-    description: {
-      story:
-        'Combining PrimaryNavigationExpansionItem and PrimaryNavigationSubItem allows for ' +
-        'consumers to create an expansion-panel-esque component in their navigation bar. ' +
-        'The current pattern is that clicking the root path will navigate users to the ' +
-        'root/dashboard view. Child routes navigation items are then rendered below. ' +
-        'Navigating away from the "rootParentPath" closes the expansion-panel route item.',
-    },
+        <Text color="inverse">Optional Header Section</Text>
+      </div>
+    ),
+    footer: (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          color: 'white',
+          width: '100%',
+          border: '1px dashed white',
+        }}
+      >
+        <Text color="inverse">Optional Footer Section</Text>
+      </div>
+    ),
+    headerMin: <b>MIN</b>,
   },
 };
-PrimaryNavigationSubrouting.args = {
-  header: (
-    <div
-      style={{
-        color: 'white',
-        display: 'flex',
-        justifyContent: 'center',
-        width: '100%',
-        border: '1px dashed white',
-      }}
-    >
-      <Text color="inverse">Optional Header Section</Text>
-    </div>
-  ),
-  footer: (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        color: 'white',
-        width: '100%',
-        border: '1px dashed white',
-      }}
-    >
-      <Text color="inverse">Optional Footer Section</Text>
-    </div>
-  ),
-  headerMin: <b>MIN</b>,
+
+export const PrimaryNavigationSubrouting: Story = {
+  render: (args) => {
+    return (
+      <PrimaryNavigation {...args}>
+        <PrimaryNavigationItem to="/edit" icon={<Edit />} label="Nav Item" />
+        <PrimaryNavigationItem
+          to="/left"
+          icon={<ArrowLeft />}
+          label="Nav Item 2"
+          betaLabelText="Beta"
+        />
+        <PrimaryNavigationExpansionItem
+          icon={<Subjects />}
+          label="Base Route"
+          to="/base"
+          rootParentPath="/base"
+        >
+          <PrimaryNavigationSubItem label="Child Route1" to="/base/c1" />
+          <PrimaryNavigationSubItem label="Child Route2" to="/base/c2" />
+        </PrimaryNavigationExpansionItem>
+      </PrimaryNavigation>
+    );
+  },
+  parameters: {
+    docs: {
+      description: {
+        story:
+          'Combining PrimaryNavigationExpansionItem and PrimaryNavigationSubItem allows for ' +
+          'consumers to create an expansion-panel-esque component in their navigation bar. ' +
+          'The current pattern is that clicking the root path will navigate users to the ' +
+          'root/dashboard view. Child routes navigation items are then rendered below. ' +
+          'Navigating away from the "rootParentPath" closes the expansion-panel route item.',
+      },
+    },
+  },
+  args: {
+    header: (
+      <div
+        style={{
+          color: 'white',
+          display: 'flex',
+          justifyContent: 'center',
+          width: '100%',
+          border: '1px dashed white',
+        }}
+      >
+        <Text color="inverse">Optional Header Section</Text>
+      </div>
+    ),
+    footer: (
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          color: 'white',
+          width: '100%',
+          border: '1px dashed white',
+        }}
+      >
+        <Text color="inverse">Optional Footer Section</Text>
+      </div>
+    ),
+    headerMin: <b>MIN</b>,
+  },
 };
