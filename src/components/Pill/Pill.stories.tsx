412d3a7eb77072e8c065dd52e2338a6341370614
Sam <samantha.aran@lifeomic.com>
2023-08-29 15:41:01 -0700

    add pill stories

--- src/components/Pill/Pill.stories.tsx
+++ src/components/Pill/Pill.stories.tsx
@@ -4,12 +4,23 @@ import { Pill } from './Pill';
 
 const meta: Meta<typeof Pill> = {
   component: Pill,
+  args: {
+    label: 'Pill',
+  },
 };
 export default meta;
 type Story = StoryObj<typeof Pill>;
 
-export const Default: Story = {
+export const Default: Story = {};
+
+export const Color: Story = {
   args: {
-    label: 'Pill',
+    color: 'negative',
+  },
+};
+
+export const Variant: Story = {
+  args: {
+    variant: 'highlight',
   },
 };
