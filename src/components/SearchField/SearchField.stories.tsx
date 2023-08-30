17c04a83450d73ea77aadc1a71085c86f8378cb3
Sam <samantha.aran@lifeomic.com>
2023-08-30 14:16:03 -0700

    fix: Update first batch of S- Components to Storybook CSF3

--- src/components/SearchField/SearchField.stories.tsx
+++ src/components/SearchField/SearchField.stories.tsx
@@ -1,40 +1,33 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { StoryObj, Meta } from '@storybook/react';
 
 import { SearchField } from './SearchField';
 
-export default {
+const meta: Meta<typeof SearchField> = {
   title: 'Form Components/SearchField',
   component: SearchField,
-  argTypes: {},
-} as ComponentMeta<typeof SearchField>;
+  args: {
+    'aria-label': 'Search',
+  },
+};
+export default meta;
+type Story = StoryObj<typeof SearchField>;
 
-const Template: ComponentStory<typeof SearchField> = (args) => (
-  <SearchField {...args} />
-);
+export const Default: Story = {};
 
-export const Default = Template.bind({});
-Default.args = {
-  'aria-label': 'Search',
+export const InverseDark = {
+  parameters: {
+    backgrounds: { default: 'dark' },
+  },
+  args: {
+    color: 'inverse',
+  },
 };
 
-export const WithLabel = Template.bind({});
-WithLabel.args = {};
-
-export const InverseDark = Template.bind({});
-InverseDark.parameters = {
-  backgrounds: { default: 'dark' },
-};
-InverseDark.args = {
-  'aria-label': 'Search',
-  color: 'inverse',
-};
-
-export const InverseBlue = Template.bind({});
-InverseBlue.parameters = {
-  backgrounds: { default: 'blue' },
-};
-InverseBlue.args = {
-  'aria-label': 'Search',
-  color: 'inverse',
+export const InverseBlue = {
+  parameters: {
+    backgrounds: { default: 'blue' },
+  },
+  args: {
+    color: 'inverse',
+  },
 };
--- src/components/SecondaryNavigation/SecondaryNavigation.stories.tsx
+++ src/components/SecondaryNavigation/SecondaryNavigation.stories.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { StoryObj, StoryFn, Meta } from '@storybook/react';
 
 import { SecondaryNavigation } from './SecondaryNavigation';
 import { MemoryRouter } from 'react-router-dom';
 import { SecondaryNavigationItem } from './SecondaryNavigationItem';
 
-export default {
-  title: 'Components/SecondaryNavigation/SecondaryNavigation',
+const meta: Meta<typeof SecondaryNavigation> = {
   component: SecondaryNavigation,
-  argTypes: {},
   decorators: [(story) => <MemoryRouter>{story()}</MemoryRouter>],
-} as ComponentMeta<typeof SecondaryNavigation>;
+};
+export default meta;
+type Story = StoryObj<typeof SecondaryNavigation>;
 
-const Template: ComponentStory<typeof SecondaryNavigation> = (args) => (
+const Template: StoryFn<typeof SecondaryNavigation> = (args) => (
   <SecondaryNavigation {...args}>
     <SecondaryNavigationItem to="/link-1" label="Link 1" />
     <SecondaryNavigationItem to="/link-2" label="Link 2" />
   </SecondaryNavigation>
 );
 
-export const Default = Template.bind({});
-Default.args = {};
+export const Default: Story = {
+  render: Template,
+};
