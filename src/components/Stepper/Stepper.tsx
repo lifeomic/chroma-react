73a6714c7a224f0455d82c8d3cc63902f9bece3e
Casey Dexter <casey.dexter@lifeomic.com>
2023-08-30 13:05:38 -0400

    feat: Add margin, padding, height and width props to Stepper

--- src/components/Stepper/Stepper.tsx
+++ src/components/Stepper/Stepper.tsx
@@ -13,6 +13,11 @@ export type StepperClasses = GetClasses<typeof useStyles>;
 
 export const useStyles = makeStyles<StepperProps>(
   (theme) => {
+    const stringOrThemeSpacing = (value: string | number | undefined) => {
+      if (typeof value === 'string') return value;
+      if (typeof value === 'number') return theme.spacing(value);
+    };
+
     const stringOrThemeColor = (value?: string) => {
       if (!value) return;
       const palette: any = theme.palette;
@@ -45,6 +50,51 @@ export const useStyles = makeStyles<StepperProps>(
         padding: 0,
         width: '100%',
       },
+      height: { height: ({ height }) => stringOrThemeSpacing(height) },
+      width: { width: ({ width }) => stringOrThemeSpacing(width) },
+      margin: { margin: ({ margin }) => stringOrThemeSpacing(margin) },
+      marginLeft: {
+        marginLeft: ({ marginLeft }) => stringOrThemeSpacing(marginLeft),
+      },
+      marginRight: {
+        marginRight: ({ marginRight }) => stringOrThemeSpacing(marginRight),
+      },
+      marginTop: {
+        marginTop: ({ marginTop }) => stringOrThemeSpacing(marginTop),
+      },
+      marginBottom: {
+        marginBottom: ({ marginBottom }) => stringOrThemeSpacing(marginBottom),
+      },
+      marginX: {
+        marginLeft: ({ marginX }) => stringOrThemeSpacing(marginX),
+        marginRight: ({ marginX }) => stringOrThemeSpacing(marginX),
+      },
+      marginY: {
+        marginTop: ({ marginY }) => stringOrThemeSpacing(marginY),
+        marginBottom: ({ marginY }) => stringOrThemeSpacing(marginY),
+      },
+      padding: { padding: ({ padding }) => stringOrThemeSpacing(padding) },
+      paddingLeft: {
+        paddingLeft: ({ paddingLeft }) => stringOrThemeSpacing(paddingLeft),
+      },
+      paddingRight: {
+        paddingRight: ({ paddingRight }) => stringOrThemeSpacing(paddingRight),
+      },
+      paddingTop: {
+        paddingTop: ({ paddingTop }) => stringOrThemeSpacing(paddingTop),
+      },
+      paddingBottom: {
+        paddingBottom: ({ paddingBottom }) =>
+          stringOrThemeSpacing(paddingBottom),
+      },
+      paddingX: {
+        paddingLeft: ({ paddingX }) => stringOrThemeSpacing(paddingX),
+        paddingRight: ({ paddingX }) => stringOrThemeSpacing(paddingX),
+      },
+      paddingY: {
+        paddingTop: ({ paddingY }) => stringOrThemeSpacing(paddingY),
+        paddingBottom: ({ paddingY }) => stringOrThemeSpacing(paddingY),
+      },
     };
   },
   { name: StepperStylesKey }
@@ -59,6 +109,22 @@ export interface StepperProps {
   onClick?: (index: number) => void;
   ref?: React.Ref<HTMLDivElement>;
   bgColor?: string;
+  height?: number | string;
+  width?: number | string;
+  margin?: number | string;
+  marginTop?: number | string;
+  marginBottom?: number | string;
+  marginLeft?: number | string;
+  marginRight?: number | string;
+  marginX?: number | string;
+  marginY?: number | string;
+  padding?: number | string;
+  paddingTop?: number | string;
+  paddingBottom?: number | string;
+  paddingLeft?: number | string;
+  paddingRight?: number | string;
+  paddingX?: number | string;
+  paddingY?: number | string;
 }
 
 /**
@@ -78,6 +144,22 @@ export const Stepper = React.forwardRef<HTMLDivElement, StepperProps>(
       className,
       connectorClassName,
       bgColor,
+      height,
+      width,
+      margin,
+      marginTop,
+      marginBottom,
+      marginLeft,
+      marginRight,
+      marginY,
+      marginX,
+      padding,
+      paddingTop,
+      paddingBottom,
+      paddingLeft,
+      paddingRight,
+      paddingY,
+      paddingX,
       onClick,
       ...rootProps
     } = props;
@@ -124,12 +206,38 @@ export const Stepper = React.forwardRef<HTMLDivElement, StepperProps>(
     return (
       <Box
         fullWidth
-        className={clsx(classes.root, bgColor && classes.bgColor, className)}
+        className={clsx(
+          classes.root,
+          height && classes.height,
+          width && classes.width,
+          bgColor && classes.bgColor,
+          className
+        )}
         ref={ref}
         role="group"
         {...rootProps}
       >
-        <ol className={classes.innerRoot}>{steps}</ol>
+        <ol
+          className={clsx(
+            classes.innerRoot,
+            margin && classes.margin,
+            marginTop && classes.marginTop,
+            marginBottom && classes.marginBottom,
+            marginLeft && classes.marginLeft,
+            marginRight && classes.marginRight,
+            marginX && classes.marginX,
+            marginY && classes.marginY,
+            padding && classes.padding,
+            paddingTop && classes.paddingTop,
+            paddingBottom && classes.paddingBottom,
+            paddingLeft && classes.paddingLeft,
+            paddingRight && classes.paddingRight,
+            paddingX && classes.paddingX,
+            paddingY && classes.paddingY
+          )}
+        >
+          {steps}
+        </ol>
       </Box>
     );
   }
