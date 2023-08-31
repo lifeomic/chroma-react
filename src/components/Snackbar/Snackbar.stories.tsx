149f942d282ae92eb265973aaa69edac0f06ce35
Sam <samantha.aran@lifeomic.com>
2023-08-31 13:42:18 -0700

    add snackbar story

--- src/components/Snackbar/Snackbar.stories.tsx
+++ src/components/Snackbar/Snackbar.stories.tsx
@@ -3,6 +3,7 @@ import { StoryObj, Meta } from '@storybook/react';
 
 import { Snackbar } from './Snackbar';
 import { Button } from '../Button';
+import { AlertTriangle } from '@lifeomic/chromicons';
 
 const meta: Meta<typeof Snackbar> = {
   component: Snackbar,
@@ -15,8 +16,35 @@ const meta: Meta<typeof Snackbar> = {
       </>
     ),
   },
+  decorators: [
+    (story: Function) => (
+      <div
+        style={{
+          height: '100px',
+        }}
+      >
+        {story()}
+      </div>
+    ),
+  ],
 };
 export default meta;
 type Story = StoryObj<typeof Snackbar>;
 
 export const Default: Story = {};
+export const Icon: Story = {
+  args: {
+    icon: AlertTriangle,
+    statusType: 'error',
+    role: 'alert',
+    allowDismiss: true,
+    children: (
+      <>
+        <span>Warning</span>
+        <Button style={{ marginLeft: 8 }} color="negative">
+          Button
+        </Button>
+      </>
+    ),
+  },
+};
