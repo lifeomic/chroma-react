bcaa29f37cd447dbb9e5279d657238d4687b309f
Sam <samantha.aran@lifeomic.com>
2023-08-31 16:29:44 -0700

    fix: Update fourth round of S- Components to Storybook CSF3

--- src/components/Stepper/Stepper.stories.tsx
+++ src/components/Stepper/Stepper.stories.tsx
@@ -1,18 +1,67 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { StoryFn, Meta, StoryObj } from '@storybook/react';
 
 import { Stepper } from './Stepper';
 import { Step } from './Step';
 import { Activity, Flag, User, Zap } from '@lifeomic/chromicons';
 
-export default {
-  title: 'Components/Stepper',
+const meta: Meta<typeof Stepper> = {
   component: Stepper,
-  argTypes: {},
-  subcomponents: { Step },
-} as ComponentMeta<typeof Stepper>;
+  argTypes: {
+    height: {
+      control: { type: 'number' },
+    },
+    width: {
+      control: { type: 'number' },
+    },
+    margin: {
+      control: { type: 'number' },
+    },
+    marginTop: {
+      control: { type: 'number' },
+    },
+    marginBottom: {
+      control: { type: 'number' },
+    },
+    marginLeft: {
+      control: { type: 'number' },
+    },
+    marginRight: {
+      control: { type: 'number' },
+    },
+    marginX: {
+      control: { type: 'number' },
+    },
+    marginY: {
+      control: { type: 'number' },
+    },
+    padding: {
+      control: { type: 'number' },
+    },
+    paddingTop: {
+      control: { type: 'number' },
+    },
+    paddingBottom: {
+      control: { type: 'number' },
+    },
+    paddingLeft: {
+      control: { type: 'number' },
+    },
+    paddingRight: {
+      control: { type: 'number' },
+    },
+    paddingX: {
+      control: { type: 'number' },
+    },
+    paddingY: {
+      control: { type: 'number' },
+    },
+  },
+};
+export default meta;
+type Story = StoryObj<typeof Stepper>;
 
-const Template: ComponentStory<typeof Stepper> = (args) => (
+const Template: StoryFn<typeof Stepper> = (args) => (
   <Stepper {...args}>
     <Step icon={Zap} subTitle="950" title="Registered" />
     <Step icon={Activity} subTitle="900" title="Consented" />
@@ -25,7 +74,9 @@ const Template: ComponentStory<typeof Stepper> = (args) => (
   </Stepper>
 );
 
-export const Default = Template.bind({});
-Default.args = {
-  activeStep: 1,
+export const Default: Story = {
+  render: Template,
+  args: {
+    activeStep: 1,
+  },
 };
